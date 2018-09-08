81c5c10b998e03ca23b96b72631528cb3e1ca9b1
Giulio Ungaretti <giulioungaretti@me.com>
2018-09-08 11:26:19 +0200

    feat: add sql provider and watch the world burn
    NETCORE and type provider are still borken ?

--- src/MyConsoleApp/Program.fs
+++ src/MyConsoleApp/Program.fs
@@ -2,11 +2,32 @@
 open Suave.Filters
 open Suave.Successful
 open Suave.Operators
+open FSharp.Data.Sql
 
 let app = 
     path "/" >=> OK "Hello"
 
 
+let [<Literal>] connectionString = "Host=localhost;Database=amie;Username=amie;Password=decaponz"
+
+type sql = SqlDataProvider<
+              ConnectionString = connectionString,
+              DatabaseVendor = Common.DatabaseProviderTypes.POSTGRESQL,
+              IndividualsAmount = 1000,
+              UseOptionTypes = true >
+let ctx = sql.GetDataContext()
+
+
+let leaf = 
+
+    query {
+         for l in ctx.Public.Leaf do
+             select l
+             }
+    |> Seq.toArray
+
+
+[<EntryPoint>]
 [<EntryPoint>]
 let main _ =
     printfn "Hello World from F#!"
